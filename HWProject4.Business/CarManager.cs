dd6d1efd58080dff8a3ce05fc9839fdecea62a3c
Bilge Yaşar <35974596+bilgeyasar@users.noreply.github.com>
2021-04-05 16:22:48 +0300

    Add files via upload

---
+++ HWProject4.Business/CarManager.cs
@@ -0,0 +1,22 @@
+﻿using HWProject4.DataAccess;
+using HWProject4.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HWProject4.Business
+{
+    public class CarManager
+    {
+        IRentACarDal _cardal;
+        public CarManager(IRentACarDal cardal)
+        {
+            _cardal = cardal;
+        }
+
+        public List<RentACar> GetRentACars()
+        {
+            return _cardal.GetRentACars();
+        }
+    }
+}
